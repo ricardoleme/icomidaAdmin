0bfad02fdb4fa1303847fbf2caed2a870bf69006
Ricardo Leme <ricardo.leme@gmail.com>
2021-05-06 17:14:41 -0300

    Inclusão de Categorias

--- src/screens/ListaCategorias.js
+++ src/screens/ListaCategorias.js
@@ -5,78 +5,87 @@ import Header from '../components/Header'
 import { BACKEND } from '../constants'
 import ListaCategoria from './ListaCategoria'
 
-function ListaCategorias({navigation, theme}){
+function ListaCategorias({ navigation, theme }) {
     const { colors } = theme
     const [categorias, setCategorias] = useState([])
     const [carregandoCategorias, setCarregandoCategorias] = useState(false)
     const [refreshing, setRefreshing] = useState(false)
 
-    useEffect(()=> {
+    useEffect(() => {
         obterCategorias()
-    },[])
+    }, [])
 
-    async function obterCategorias(){
+    async function obterCategorias() {
         setCarregandoCategorias(true)
         let url = `${BACKEND}/categorias`
         await fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            setCategorias(data)
-            //console.log('Categorias obtidas com sucesso!')
-        })
-        .catch(function (error) {
-            console.error(`Houve um problema ao obter as categorias: ${error.message}`)
-        })
+            .then(response => response.json())
+            .then(data => {
+                setCategorias(data)
+                //console.log('Categorias obtidas com sucesso!')
+            })
+            .catch(function (error) {
+                console.error(`Houve um problema ao obter as categorias: ${error.message}`)
+            })
         setCarregandoCategorias(false)
     }
 
-    const onRefresh = React.useCallback(async ()=> {
+    const onRefresh = React.useCallback(async () => {
         setRefreshing(true)
-        try{
+        try {
             await obterCategorias()
-        } catch (error){
+        } catch (error) {
             console.error(error)
         }
         setRefreshing(false)
-    },[refreshing])
+    }, [refreshing])
 
     return (
         <>
-        <Header titulo="Categorias de Restaurantes" back={true} navigation={navigation} />
-        <View style={{backgroundColor: colors.surface, paddingHorizontal: 8,
-        paddingVertical: 8, flex:1}}>
-            <List.Subheader>
-               <Avatar.Icon size={24} icon="refresh"/> Para atualizar os dados 
+            <Header titulo="Categorias de Restaurantes" back={true} navigation={navigation} />
+            <View style={{
+                backgroundColor: colors.surface, paddingHorizontal: 8,
+                paddingVertical: 8, flex: 1
+            }}>
+                <List.Subheader>
+                    <Avatar.Icon size={24} icon="refresh" /> Para atualizar os dados
             </List.Subheader>
 
-            {carregandoCategorias && <ActivityIndicator size="large" color={colors.primary} />}
-            {categorias.length === 0 && !carregandoCategorias ?
-            (
-                <View style={styles.tituloAviso}>
-                    <Text style={styles.titulo}>Ainda não há nenhuma categoria cadastrada.</Text>
-                </View>
-            ) : (
-                <FlatList 
-                    data={categorias}
-                    renderItem={({item}) => (
-                        <ListaCategoria data={item} navigation={navigation} />
-                    )}
-                    keyExtractor={item => item._id.toString()}
-                    refreshControl={<RefreshControl 
-                                        refreshing={refreshing}
-                                        onRefresh={onRefresh} />
+                {carregandoCategorias && <ActivityIndicator size="large" color={colors.primary} />}
+                {categorias.length === 0 && !carregandoCategorias ?
+                    (
+                        <View style={styles.tituloAviso}>
+                            <Text style={styles.titulo}>Ainda não há nenhuma categoria cadastrada.</Text>
+                        </View>
+                    ) : (
+                        <FlatList
+                            data={categorias}
+                            renderItem={({ item }) => (
+                                <ListaCategoria data={item} navigation={navigation} />
+                            )}
+                            keyExtractor={item => item._id.toString()}
+                            refreshControl={<RefreshControl
+                                refreshing={refreshing}
+                                onRefresh={onRefresh} />
+                            }
+                        />
+                    )
                 }
+                <FAB
+                    style={styles.fab}
+                    icon='plus'
+                    label=''
+                    onPress={() => navigation.navigate('AdicionaCategoria', {
+                        data: {
+                            _id: null,
+                            nome: '',
+                            status: true,
+                            foto: {originalname: '', path: '', size: 0, mimetype: ''}
+                        }
+                    })}
                 />
-            )
-            }
-                  <FAB
-              style={styles.fab}
-              icon='plus'
-              label=''
-              onPress={()=> navigation.navigate('AdicionaCategoria')}
-              />  
-        </View>
-        
+            </View>
+
         </>
     )
 }
